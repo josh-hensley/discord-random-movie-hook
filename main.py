cfb920790629ffed08d51745b36362cdaee9f6b6
josh-hensley <me@joshhensley.com>
2026-04-02 11:51:11 -0500

    changed minimum votes to 500

--- main.py
+++ main.py
@@ -12,9 +12,36 @@ TMDB_API_KEY = getenv('TMDB_API_KEY')
 DISCORD_WEB_HOOK = getenv('DISCORD_WEB_HOOK')
 TEST_HOOK = getenv('TEST_HOOK')
 
-with open('genres.json') as file:
-    data = json.load(file)
-    genres = data["genres"]
+genres = [
+		{
+			"id": "28%7C12",
+			"name": "Action/ Adventure"
+		},
+		{
+			"id": 35,
+			"name": "Comedy"
+		},
+		{
+			"id": 99,
+			"name": "Documentary"
+		},
+		{
+			"id": "18%7C9648",
+			"name": "Drama/ Mystery"
+		},
+		{
+            "id": 10749,
+			"name": "Romance"
+		},
+		{
+            "id": 878,
+			"name": "Science Fiction"
+		},
+        {
+            "id": "27%7C53",
+            "name": "Horror/ Thriller"
+        }
+	]
     
 date = datetime.datetime.now()
 day = date.weekday()
@@ -33,17 +60,17 @@ response = requests.get(url, headers=headers)
 
 json = response.json()
 
-randomEntry = random.randrange(1, json['total_results'])
+randomEntry = random.randrange(0, json['total_results'] - 1)
 pageItem = randomEntry % 20
 page = math.floor(randomEntry / 20) + 1
 print(f"random entry: {randomEntry}\npage: {page}\nitem: {pageItem}")
 
-url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=vote_average.desc&vote_average.gte=5&vote_count.gte=1000&with_genres={genre["id"]}&page={page}'
+url = f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=vote_average.desc&vote_average.gte=5&vote_count.gte=500&with_genres={genre["id"]}&page={page}'
 
 response = requests.get(url, headers=headers)
 
 json = response.json()
-movie = json['results'][pageItem] if json['results'][pageItem] else json['results'][0]
+movie = json['results'][pageItem]
 title = movie['title']
 releaseDate = movie['release_date'].split('-')[0]
 plot = movie['overview']
